4f8b4cbdbf1c6869fe152d38bbfa4f316cf9933e
Anthony Johnson <beatsbyseptember@gmail.com>
2020-10-20 21:29:00 -0400

    images add using axios and API

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
     <div className="App">
       <h1>Hello Anthony</h1>
       <Row title="Netflix Originals" fetchUrl={request.fetchNetflixOriginals}/>
-      <Row title="Trending Now" etchUrl={request.fetchTrending}/>
+      <Row title="Trending Now" fetchUrl={request.fetchTrending}/>
+      <Row title="Action" fetchUrl={request.fetchActionMovies}/>
+      <Row title="Comedy" fetchUrl={request.fetchComedyMovies}/>
+      <Row title="Horror" fetchUrl={request.fetchHorrorMovies}/>
+      <Row title="Romance" fetchUrl={request.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React,  {useState, useEffect} from 'react';
-import axios from './asxios'
+import axios from './asxios';
+import './Row.css';
+
+const base_url = " https://image.tmdb.org/t/p/original/";
  
 function Row({title, fetchUrl}) {
     const [newMovies, setNewMovies] = useState([]);
@@ -14,10 +17,21 @@ function Row({title, fetchUrl}) {
 
         fetchData();
     }, [fetchUrl]);
+    console.log(newMovies)
 
     return(
-        <div>
+        <div className="row">
+
             <h2>{title}</h2>
+
+            <div className="rowPosters">
+
+                {newMovies.map( movie => (
+                   <img className="rowPoster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+
+
+            </div>
 
         </div>
     )
